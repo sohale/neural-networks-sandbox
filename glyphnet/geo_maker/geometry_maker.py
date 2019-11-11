f9b1e0f1732ef6261cb2833bf5cc05dd1a1ac573
sohale <Sohail.Siadatnejad@lloydsbanking.com>
2019-11-11 19:30:39 +0000

    produces something. rubbish but ok

--- glyphnet/geo_maker/geometry_maker.py
+++ glyphnet/geo_maker/geometry_maker.py
@@ -23,13 +23,22 @@ import numpy as np
 from skimage.draw import line_aa
 
 def simple_triangles(size_shape):
-    img = np.zeros((14, 14), dtype=np.uint8)
-    rr, cc, val = line_aa(1, 1, 8, 4)
-    rr, cc, val = line_aa(0, 13, 13, 4)
-    img[rr, cc] = val * 255
-    # scipy.misc.imsave("out.png", img)
-    img_rgb = np.repeat(img[:,:, None], 3, axis=2)
-    return [img_rgb.flatten()]
+    W,H = 14,14
+    those = []
+    for i in range(10):
+        img = np.zeros((W, H), dtype=np.uint8)
+        rr, cc, val = line_aa(1, 1, 8, 4)
+        img[rr, cc] = val * 255
+        rr, cc, val = line_aa(0, 13, 13, 4)
+        img[rr, cc] = val * 255
+
+        rr, cc, val = line_aa(0+i, 13-i, 13-i, 4)
+        img[rr, cc] = val * 255
+
+        # scipy.misc.imsave("out.png", img)
+        img_rgb = np.repeat(img[:,:, None], 3, axis=2)
+        those.append(img_rgb.flatten())
+    return those
 
     #img = np.random.randn(64, size_shape)
     #return img
