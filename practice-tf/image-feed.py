18b86b1b6492d379a25a0208e47828a5155a8307
Sohail Siadatnejad <sohale@gmail.com>
2019-12-22 00:54:39 +0000

    practice: batch size of 2 ( > 1 )

--- practice-tf/image-feed.py
+++ practice-tf/image-feed.py
@@ -11,22 +11,26 @@ import imageio
 np.set_printoptions(threshold=5, linewidth=80, precision=1)
 
 
-RGB3DIMS = 3
+RGB3DIMS = 1
 W = 2
-H = 4
+H = 3
+BATCHSIZE = 2
 
 # wiring up the circuit
-input = tf.placeholder(tf.uint8, [None, None, RGB3DIMS])
+input = tf.placeholder(tf.uint8, [BATCHSIZE, W, H, RGB3DIMS])
+# input = tf.placeholder(tf.uint8, [None, W, H, RGB3DIMS])
 output = input * 2
 
 # input data
 
-#data_im1 = np.zeros(img_shape, dtype=np.uint8)
+#data_img = np.zeros(img_shape, dtype=np.uint8)
 
 img_shape = (W,H, RGB3DIMS)
 img_shape1 = (W, 1, RGB3DIMS)
-row = np.arange(H, dtype=np.uint8) [None,:,None]
-data_im1 = np.tile( row, img_shape1)
+row = np.arange(H, dtype=np.uint8)
+data_img = np.tile( row[None,:,None], img_shape1)
+
+data_images_batch = np.tile( data_img[None, :,:,:], [BATCHSIZE, 1,1,1])
 
 
 
@@ -34,10 +38,10 @@ data_im1 = np.tile( row, img_shape1)
 
 sess = tf.Session()
 (out_data,) = \
-    sess.run([output], feed_dict={input: data_im1})
+    sess.run([output], feed_dict={input: data_images_batch})
 
 # report the results
-print('input')
-print(data_im1)
-print('output')
+print('Input: ---------------')
+print(data_images_batch)
+print('Output: ---------------')
 print(out_data)
