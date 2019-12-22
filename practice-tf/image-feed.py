a0df34d87620a0074aa8abeb40fb888061635f8f
Sohail Siadatnejad <sohale@gmail.com>
2019-12-22 00:44:49 +0000

    practice: a decent WxHxRGB input

--- practice-tf/image-feed.py
+++ practice-tf/image-feed.py
@@ -13,14 +13,26 @@ import imageio
 RGB3DIMS = 3
 
 
-input = tf.placeholder(tf.uint8, [None, None, RGB3DIMS]) 
+input = tf.placeholder(tf.uint8, [None, None, RGB3DIMS])
 output = input * 2
 
-data_im1 = np.ones([5,4,3], dtype=np.uint8)
+img_shape = (2,4,3)
+img_shape1 = (img_shape[0], 1, img_shape[2])
+#data_im1 = np.zeros(img_shape, dtype=np.uint8)
+row = np.arange(img_shape[1], dtype=np.uint8) [None,:,None]
+data_im1 = np.tile( row, img_shape1)
+
+print(row.shape)
+print(data_im1.shape)
+
+np.set_printoptions(threshold=5, linewidth=80, precision=1)
 
 
 sess = tf.Session()
-out_data = \
+(out_data,) = \
     sess.run([output], feed_dict={input: data_im1})
 
+print('input')
+print(data_im1)
+print('output')
 print(out_data)
