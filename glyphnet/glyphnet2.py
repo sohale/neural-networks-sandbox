2fcb1fceae54cd8fa2bb0013eb0887a2db5e733b
Sohail Siadatnejad <sohale@gmail.com>
2019-12-25 00:07:03 +0000

    convolution offser-range AND stride

--- glyphnet/glyphnet2.py
+++ glyphnet/glyphnet2.py
@@ -65,10 +65,22 @@ HL_DTYPE = tf.float32
 WEIGHT_DTYPE = tf.float32
 
 
-def make_conv_rf(input, SHAPE, RF1, nonlinearity1, lname):
+def make_conv_rf(input, SHAPE, conv_offset_range, stride_xy, nonlinearity1, lname):
     (W,H,RGB3DIMS) = SHAPE
     print('input.shape for', lname, input.shape, ' asserting', tuple(input.shape[1:]), '==', (W,H,RGB3DIMS))
     assert tuple(input.shape[1:]) == (W,H,RGB3DIMS), """ explicitl specified SHAPE (size) must match %s. """ % (repr(input.shape[1:]))
+
+    # RF1 -> conv_offset_range
+
+    assert isinstance(SHAPE[0], int)
+    assert isinstance(SHAPE[1], int)
+    assert isinstance(SHAPE[2], int)
+    assert isinstance(stride_xy[0], int)
+    assert isinstance(stride_xy[1], int)
+    assert len(stride_xy) == 2
+    assert isinstance(conv_offset_range[0], int)
+    assert isinstance(conv_offset_range[1], int)
+    assert len(conv_offset_range) == 2
 
     #NEWSHAPE = (W-RF1+1,H-RF1+1,RGB3DIMS)
     #assert W-RF1+1 > 0, """RF size %d does not fit in W=%d""" % (RF1, W)
@@ -87,14 +99,15 @@ def make_conv_rf(input, SHAPE, RF1, nonlinearity1, lname):
       # note: the smaller range has nothing to do with the next layer shrinking smaller.
       # the output unit needs to have a center-of-RF, based on which we reduce the layer. We depart from convensional, just here.
       # (x,y) is also the coords of the location.
-      for x in range(W):
-        for y in range(H):
+      for x in range(0, W, stride_xy[0]):
+        for y in range(0, H, stride_xy[1]):
           cuname1 = "x%dy%d"%(x,y)
           with tf.variable_scope(cuname1):
             #for c in range(RGB3DIMS):
-            suminp = None
-            for dx in range(RF1):
-                for dy in range(RF1):
+            #suminp = None
+            suminp = 0.0
+            for dx in range(conv_offset_range[0], conv_offset_range[1], 1):
+                for dy in range(conv_offset_range[0], conv_offset_range[1], 1):
                     # print('x,y,dx,dy  ', x,y,dx,dy, '  + -> ', x+dx, y+dy)
                     # coords and index on input layer.
                     inp_x, inp_y = x+dx, y+dy
@@ -108,9 +121,10 @@ def make_conv_rf(input, SHAPE, RF1, nonlinearity1, lname):
                     (x,y,c) = get_element_metadata(input, inp_x,inp_y)
                     randinitval = tf.random_uniform([1], -1, 1, seed=0)  # doesn accept trainable=False,
                     w1 = tf.Variable(initial_value=randinitval, trainable=True, dtype=WEIGHT_DTYPE)
-                    if suminp is None:
-                        suminp = w1 * v1
-                    else:
+                    #if suminp is None:
+                    #    suminp = w1 * v1
+                    #else:
+                    if True:
                         suminp = suminp + w1 * v1
             b1 = tf.Variable(initial_value=0.0, trainable=True, dtype=HL_DTYPE)
             suminp = suminp + b1
@@ -122,7 +136,7 @@ def make_conv_rf(input, SHAPE, RF1, nonlinearity1, lname):
       layer_h1 = tf.concat(ll, axis=1) # row: (W*H) x RGB3
 
       #NEWRESHAPE = [-1, W-RF1+1,H-RF1+1,RGB3DIMS]
-      NEWRESHAPE = [-1, W,H,RGB3DIMS]
+      NEWRESHAPE = [-1, int(W/stride_xy[0]), int(H/stride_xy[1]), RGB3DIMS]
       reshaped_hidden_layer = tf.reshape(layer_h1, NEWRESHAPE)
 
       set_metadata_bulk(W,H, reshaped_hidden_layer)
@@ -141,8 +155,8 @@ H = 5 #15
 BATCHSIZE = 4 #2
 
 # receptive field size
-RF1 = 4 #3
-RF2 = 2
+RF1 = int(4/2) #3
+RF2 = int(2/2)
 
 input = tf.placeholder(PIXEL_DTYPE, [None, W, H, RGB3DIMS], name='i1')
 #reshp = tf.reshape(input, [UNKNOWN_SIZE, W*H, RGB3DIMS])
@@ -152,9 +166,9 @@ set_metadata_bulk(W,H, input)
 nonlinearity1 = tf.nn.relu
 #nonlinearity1 = tf.sigmoid
 print('L1')
-layer_h1 = make_conv_rf(input, (W,H,RGB3DIMS), RF1, tf.nn.relu, lname='H1')
+layer_h1 = make_conv_rf(input, (W,H,RGB3DIMS),  (-RF1,RF1), (1,1), tf.nn.relu, lname='H1')
 print('L2')
-layer_h2 = make_conv_rf(layer_h1, (W,H,RGB3DIMS), RF2, tf.nn.relu, lname='H2')
+layer_h2 = make_conv_rf(layer_h1, (int(W/1),int(H/1),RGB3DIMS), (-RF2, RF2), (2,2), tf.nn.relu, lname='H2')
 
 
 
